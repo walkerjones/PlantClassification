816e4391042e4e6fdbed2e1fa84efc2131272495
Łukasz Wiktor <lukaszmwiktor@gmail.com>
2021-08-17 02:58:43 +0200

    extended first test

--- flowertest.py
+++ flowertest.py
@@ -1,20 +1,23 @@
 
 import tensorflow as tf
 from tensorflow import keras
+import os
 
-image_size = (180, 180) 
 model = keras.models.load_model('flowers_98.h5')
 
-img = keras.preprocessing.image.load_img(
-    "flowertest/dmucha.jpg", target_size=image_size
-)
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+folder_path="flowertest"
+for fname in os.listdir(folder_path):
+    fpath = os.path.join(folder_path, fname)
+    image_size = (180, 180) 
+    img = keras.preprocessing.image.load_img(fpath, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
+    predictions = model.predict(img_array)
+    score = predictions
 
-
-predictions = model.predict(img_array)
-score = predictions
-class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
-for i in range(5):
-    print('%12s' % class_names[i],":", ('{:.1%}'.format(score[0,i])))
+    print(" Inference on: ", fpath)
+    class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+    for i in range(5):
+        print('%12s' % class_names[i],":", ('{:.1%}'.format(score[0,i])))
